8a09a76f77327813e0599b5702b224c12993c604
VigneshwaranR-Hexaware <vigneshwaranr.hexaware@gmail.com>
2017-08-28 11:53:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ intents.matches('health-addons', [
         console.log("health-addons Intend triggered")
         console.log("Args : "+JSON.stringify(args));
         var responseString="Sure Mike. Based on your job profile, team and dependent information"+
-                           "HexaHealth recommends the following h-commerce plans for you"+ ""
-        var healthPackage="1.Emergency Back Up Home Nurse Care for dependents 50 hrs 24x7 home care at $650." + "" +
+                           "HexaHealth recommends the following h-commerce plans for you <br>"+ 
+        var healthPackage="1.Emergency Back Up Home Nurse Care for dependents 50 hrs 24x7 home care at $650.<br>" + 
                           "2.Intelligent Home Monitoring system for elderly Monthly rental plan $350"
         var msg=responseString + healthPackage;
         session.send(msg);
